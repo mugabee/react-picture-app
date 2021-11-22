9be9e218887575f638d93f724da91f8b20eded80
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-22 22:08:31 +0200

    Project StartUp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import react from "react";
+import React, { useState, useEffect } from 'react';
 import Header from "./component/Header";
 import Loader from "./component/Loader";
 import UnsplashImage from "./component/UnsplashImage";
+import axios from 'axios';
 
 
 function App() {
+  const [images, setImage] = useState([]);
+  const accessKey = process.env.REACT_APP_ACCESSKEY;
+  useEffect(() => {
+    const getApi = "https://api.unsplash.com";
+
+    axios
+    .get(`${getApi}/photos/random?client_id=${accessKey}&count=${count}`)
   return (
     <div>
       <Header />
