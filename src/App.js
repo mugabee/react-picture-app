e0a6f944fd086703c790248c574bded334e1c370
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-22 22:18:18 +0200

    Project StartUp

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 
 
 function App() {
-  const [images, setImage] = useState([]);
-  ;
+  // const [images, setImage] = useState([]);
+  // ;
   useEffect(() => {
     const getApi = "https://api.unsplash.com";
     const getAccessKey = process.env.REACT_APP_ACCESSKEY
@@ -16,7 +16,7 @@ function App() {
     .get(`${getApi}/photos/random?client_id=${getAccessKey}&count=10`)
     .then(res => console.log(res.data));
     })
-    
+
   return (
     <div>
       <Header />
