fc1f986f27a6e83a6210b51e23ea7fb78ed7053f
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-23 15:33:37 +0200

    building up the search bar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,14 @@
 
 const Header = () => {
     return (
-        <div>
-            heading
+        <div className="flex py-3 items-center justify-center ">
+        <div className="flex border-2 border-gray-200 rounded">
+            <input type="text" className="px-4 text-black py-2 w-80" placeholder="Search..."/>
+            <button className="px-4 text-white bg-gray-600 border-l ">
+                Search for the image
+            </button>
         </div>
+    </div>
     )
 }
 
