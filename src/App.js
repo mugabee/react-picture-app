4aed36487245b6b7b28286746ef8ba754a646d3b
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-23 15:59:10 +0200

    Search Functionality

--- src/App.js
+++ src/App.js
@@ -7,27 +7,67 @@ import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-function App() {
-  // const [images, setImage] = useState([]);
-  // ;
+  body {
+    font-family: sans-serif;
+  }
+`;
+
+const WrapperImages = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-auto-rows: 300px;
+`;
+
+
+const App = () => {
+
+  const [images, setImage] = useState([]);
+
   useEffect(() => {
-    const getApi = "https://api.unsplash.com";
-    const getAccessKey = process.env.REACT_APP_ACCESSKEY
+    fetchImages();
+  }, [])
+
+  const fetchImages = (count = 10) => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
 
     axios
-    .get(`${getApi}/photos/random?client_id=${getAccessKey}&count=10`)
-    .then(res => console.log(res.data));
-    })
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+      .then(res => {
+        setImage([...images, ...res.data]);
+      })
+  }
+
 
   return (
     <div>
       <Header />
-      <Loader />
-      <UnsplashImage />
+      <GlobalStyle />
+      <InfiniteScroll
+        dataLength={images.length}
+        next={fetchImages}
+        hasMore={true}
+        loader={<Loader />}
+      >
+        <WrapperImages>
+          {images.map(image => (
+            <UnsplashImage url={image.urls.thumb} key={image.id} />
+          ))}
+        </WrapperImages>
+      </InfiniteScroll>
+      
     </div>
-  );
+  )
 }
-import InfiniteScroll from 'react-infinite-scroll-component';
 
-export default App;
+export default App
--- src/component/Header.js
+++ src/component/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <div className="flex py-3 items-center justify-center ">
         <div className="flex border-2 border-gray-200 rounded">
-            <input type="text" className="px-4 text-black py-2 w-80" placeholder="Search..."/>
+            <input type="text" className="px-4 text-black py-2 w-80" placeholder="Image Search..."/>
             <button className="px-4 text-white bg-gray-600 border-l ">
-                Search for the image
+                Search 
             </button>
         </div>
     </div>
